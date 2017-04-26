ebfd980f37530a0cdca45c02e97f2d3ace4d7535
jnphilipp <me@jnphilipp.org>
2017-04-26 09:02:12 +0200

    Update HSONObserver to new BaseObserver.

--- sacred/observers/json.py
+++ sacred/observers/json.py
@@ -17,7 +17,6 @@ class JSONObserver(RunObserver):
         self.experiment_dir = None
         self.run_entry = None
 
-
     def create_dirs(self, name, _id):
         experiments_dir = os.path.join(self.base_dir, name)
         if not os.path.exists(experiments_dir):
@@ -25,13 +24,17 @@ class JSONObserver(RunObserver):
 
         if _id is None and self.nb_experiment is -1:
             self.nb_experiment = sum(1 for e in os.scandir(experiments_dir) if e.is_dir())
-            self.experiment_dir = os.path.join(self.base_dir, name, self.number_format % self.nb_experiment)
+            self.experiment_dir = os.path.join(self.base_dir,
+                                               name,
+                                               self.number_format % self.nb_experiment)
             os.makedirs(self.experiment_dir)
         elif self.nb_experiment is not -1 and self.experiment_dir is None:
-            self.experiment_dir = os.path.join(self.base_dir, name, self.number_format % self.nb_experiment)
+            self.experiment_dir = os.path.join(self.base_dir,
+                                               name,
+                                               self.number_format % self.nb_experiment)
 
-
-    def queued_event(self, ex_info, command, queue_time, config, meta_info, _id):
+    def queued_event(self, ex_info, command, queue_time, config, meta_info,
+                     _id):
         self.create_dirs(ex_info['name'], _id)
         self.run_entry = {
             '_id': self.number_format % self.nb_experiment,
@@ -44,8 +47,8 @@ class JSONObserver(RunObserver):
         self.save()
         return self.nb_experiment if _id is None else _id
 
-
-    def started_event(self, ex_info, command, host_info, start_time, config, meta_info, _id):
+    def started_event(self, ex_info, command, host_info, start_time, config,
+                      meta_info, _id):
         self.create_dirs(ex_info['name'], _id)
         self.run_entry = {
             '_id': self.number_format % self.nb_experiment,
@@ -64,12 +67,11 @@ class JSONObserver(RunObserver):
         self.save()
         return self.nb_experiment if _id is None else _id
 
-
-    def heartbeat_event(self, info, captured_out, beat_time):
+    def heartbeat_event(self, info, captured_out, beat_time, result):
         self.run_entry['info'] = info
         self.run_entry['captured_out'] = captured_out
+        self.run_entry['result'] = result
         self.save()
-
 
     def completed_event(self, stop_time, result):
         self.run_entry['stop_time'] = stop_time.isoformat()
@@ -78,7 +80,9 @@ class JSONObserver(RunObserver):
         self.save()
 
         if result:
-            experiments_path = os.path.join(self.base_dir, self.run_entry['experiment']['name'], 'experiments.csv')
+            experiments_path = os.path.join(self.base_dir,
+                                            self.run_entry['experiment']['name'],
+                                            'experiments.csv')
             fields = ['experiment'] + list(result.keys())
             experiments = []
             if os.path.exists(experiments_path):
@@ -103,12 +107,10 @@ class JSONObserver(RunObserver):
                 result['experiment'] = self.number_format % self.nb_experiment
                 writer.writerow(result)
 
-
     def interrupted_event(self, interrupt_time, status):
         self.run_entry['stop_time'] = interrupt_time.isoformat()
         self.run_entry['status'] = status
         self.save()
-
 
     def failed_event(self, fail_time, fail_trace):
         self.run_entry['stop_time'] = fail_time.isoformat()
@@ -116,28 +118,25 @@ class JSONObserver(RunObserver):
         self.run_entry['fail_trace'] = fail_trace
         self.save()
 
-
     def resource_event(self, filename):
         self.run_entry['resources'].append(filename)
         self.save()
-
 
     def artifact_event(self, name, filename):
         self.run_entry['artifacts'].append(name)
         self.save()
 
-
     def save(self):
-        with open(os.path.join(self.experiment_dir, 'sacred-%s.json' % self.run_entry['command']), 'w', encoding='utf8') as f:
+        with open(os.path.join(self.experiment_dir,
+                               'sacred-%s.json' % self.run_entry['command']),
+                  'w', encoding='utf8') as f:
             f.write(json.dumps(self.run_entry, indent=4, default=json_util.default))
             f.write('\n')
-
 
     def __eq__(self, other):
         if isinstance(other, JSONObserver):
             return self.runs == other.runs
         return False
-
 
     def __ne__(self, other):
         return not self.__eq__(other)
